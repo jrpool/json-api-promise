67e2f0ed3dc1e3e070cbe3e7e44672a3f9aa1c52
Jonathan Pool <pool@stulta.com>
2017-07-29 16:54:47 -0700

    Finished developing app.js.

--- app.js
+++ app.js
@@ -1,129 +1,95 @@
 // Create a server application.
 const app = require('express')();
 
-// Import and configure required modules.
-const textParser
-  = require('body-parser').text({inflate: false, limit: 1000, type: '*/*'});
-const fs = require('fs');
+// Import required modules.
+const rpn = require('request-promise-native');
+const cheerio = require('cheerio');
 
-// Formulate a success report.
-const reportSuccess = (verb, nextID) =>
-  `{"message": "Successfully ${verb} the file ${nextID}.json"}\n`;
+/*
+  Identify the parameters of external requests that will be made, except
+  for the query string.
+*/
+const extReqParams = {
+  url: 'http://www.imdb.com/find',
+  q: '',
+  ref_: 'nv_sr_fn',
+  s: 'all'
+};
 
 // Formulate an error report.
 const reportError = (clause, error) =>
   `Could not ${clause}.\nError: ${error.message}\n`;
 
-///// ROUTES /////
+/**
+  Define a function to extract an array of title lines from an HTML document
+  served by IMDB in response to a search.
+*/
+const titleList = body => {
+  const $ = cheerio.load(body);
+  const listCells =
+    $('a[name=tt]')
+      .parent()
+      .parent()
+      .children('table')
+      .children('tr')
+      .children('td.result_text');
+  const listTexts = [];
+  listCells.each((index, element) => {
+    listTexts.push($(element).text().replace(/^ +/, ''));
+  });
+  return listTexts;
+};
 
-// Add a quote.
-app.post(
-  '/api/quotes',
-  textParser,
-  (req, res) => {
-    // Get the next ID.
-    fs.readFile(
-      __dirname + '/util/nextid.json',
-      'utf8',
-      (err, data) => {
-        if (err) {
-          res.send(reportError('look up the next quote’s ID', err));
-        }
-        else {
-          const nextID = JSON.parse(data).nextID;
-          // Write the request body to a file named with the next ID.
-          fs.writeFile(
-            __dirname + '/public/quotes/' + nextID + '.json',
-            req.body,
-            'utf8',
-            err => {
-              if (err) {
-                res.send(reportError('record the quote', err));
-              }
-              else {
-                res.send(reportSuccess('created', nextID));
-                // Increment the next ID.
-                fs.writeFile(
-                  __dirname + '/util/nextid.json',
-                  `{"nextID": "${Number.parseInt(nextID) + 1}"}`,
-                  'utf8',
-                  err => {
-                    if (err) {
-                      res.send(reportError('increment the next ID', err));
-                    }
-                  }
-                );
-              }
-            }
-          );
-        }
-      }
-    );
-  }
-);
+/*
+  Define a function to convert an array of IMDB title lines to an indented
+  JSON string.
+*/
+const jsonify = listTexts => {
+  const listObject = {'movies': []};
+  listTexts.forEach(listText => {
+    // Ignore lines deviating from title (year) format.
+    if(/^([()])+ \((\d{4})\)/) {
+      listObject.movies.push({'name': $1, 'year': $2});
+    }
+  });
+  return JSON.stringify(listObject, null, 2);
+};
 
-// Retrieve a quote.
+// If it is:
+if (query) {
+  // Make it the value of the “q” property of requestParams.
+  requestParams['q'] = query;
+  // Identify the complete URL, including the query.
+  // Make an HTTP GET request to that URL and process the result.
+  rpn(urlWithQuery, processRequestResult);
+}
+
+////// CLIENT REQUEST ROUTES //////
+
+// Perform a search.
 app.get(
-  '/api/quotes/:id',
+  '/api/search/:q',
   (req, res) => {
-    // Get the specified quote.
-    fs.readFile(
-      __dirname + '/public/quotes/' + req.params.id + '.json',
-      'utf8',
-      (err, data) => {
-        if (err) {
-          res.send(reportError('look up the quote', err));
+    extReqParams.q = req.params.q;
+    const urlWithQuery
+      = extReqParams.url
+      + '?'
+      + ['q', 'ref_', 's'].map(val => val + '=' + extReqParams[val]).join('&');
+    rpn(urlWithQuery)
+      .then({
+        body => {
+          res.send(jsonify(titleList(body)));
         }
-        else {
-          res.send(data + '\n');
-        }
-      }
-    );
+      })
+      .catch(err => {
+        reportError('perform your search', err);
+      });
   }
 );
 
-// Replace a quote.
-app.put(
-  '/api/quotes/:id',
-  textParser,
-  (req, res) => {
-    // Write the request body to a file named with the specified ID.
-    fs.writeFile(
-      __dirname + '/public/quotes/' + req.params.id + '.json',
-      req.body,
-      'utf8',
-      err => {
-        if (err) {
-          res.send(reportError('replace the quote', err));
-        }
-        else {
-          res.send(reportSuccess('replaced', req.params.id));
-        }
-      }
-    );
-  }
-);
-
-// Delete a quote.
-app.delete(
-  '/api/quotes/:id',
-  (req, res) => {
-    // Delete the specified quote.
-    fs.unlink(
-      __dirname + '/public/quotes/' + req.params.id + '.json',
-      (err, data) => {
-        if (err) {
-          res.send(reportError('delete the quote', err));
-        }
-        else {
-          res.send(reportSuccess('deleted', req.params.id));
-        }
-      }
-    );
-  }
-);
+////// EXECUTION //////
 
 // Make the application listen for queries.
 app.listen(3000, () => {
-  console.log('App queriable at http://localhost:3000');
+  console.log('App queriable at http://localhost:3000/api/search/«query»');
 });
